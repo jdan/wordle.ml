f00caf399f9cd06c877eedd2c28fd4c3e8bdcac9
Jordan Scales <scalesjordan@gmail.com>
2022-01-15 14:05:38 -0500

    greedybot module

--- bin/dojo.ml
+++ bin/dojo.ml
@@ -2,34 +2,54 @@ open Wordle.Seacher
 open Wordle.Evaluator
 open Wordle.Util
 
-exception NoRemainingWordsException
-let run target dictionary =
-  (* initialize the bot *)
-  let freq = frequency_of_strs dictionary in
-  let rec step evaluations = function
-    | [] -> raise NoRemainingWordsException
-    | words ->
-      (* this is the bot's move *)
-      let sorted_candidates =
-        filter_words2 evaluations words
-        |> sorted_candidates freq
-        |> List.map fst
-      in (
-        match sorted_candidates with
-        | [] -> raise NoRemainingWordsException
-        | best_guess :: remaining ->
-          let evaluation = evaluate best_guess target
-          in if evaluation = [Green; Green; Green; Green; Green]
-          then [best_guess]
-          else
-            best_guess ::
-            step
-              ((best_guess, evaluation) :: evaluations)
-              remaining
-      )
-  in step [] dictionary
+module type Bot = sig
+  type t
+  val initialize : string list -> t
+  val guess : t -> string
+  val update : t -> string -> result -> t
+end
 
-let target = "gross"
+exception NoRemainingWordsException
+
+module GreedyBot : Bot = struct
+  type t =
+    { freq : (char, int) Hashtbl.t
+    ; candidates : string list
+    ; history : (string * result) list
+    }
+
+  let initialize dictionary =
+    { freq = frequency_of_strs dictionary
+    ; candidates = dictionary
+    ; history = []
+    }
+
+  let guess { freq; candidates; history } =
+    match (
+      filter_words2 history candidates
+      |> sorted_candidates freq
+    ) with
+    | [] -> raise NoRemainingWordsException
+    | (best, _) :: _ -> best
+
+  let update ({history; _} as me) attempt result =
+    (* TODO: trim down candidates *)
+    { me with
+      history = (attempt, result) :: history
+    }
+end
+
+let run target dictionary =
+  let rec step bot =
+    let guess = GreedyBot.guess bot
+    in let evaluation = evaluate guess target
+    in if evaluation = [Green; Green; Green; Green; Green]
+    then [guess]
+    else
+      guess :: step (GreedyBot.update bot guess evaluation)
+  in step (GreedyBot.initialize dictionary)
+
+let target = "hatch"
 
 let () =
   let dictionary = read_lines ()
